0e12d64809631982dcc06f47f056b37055e0a7c6
Ryder Zvorak <ryderzvorak@gmail.com>
2025-02-13 00:08:40 -0600

    changed naming

---
+++ frontend/src/pages/Scores_Class.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Scores_Class = () => {
+  return (
+    <div>Scores_Class</div>
+  )
+}
+
+export default Scores_Class
---
+++ frontend/src/pages/Scores_Type.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Scores_Type = () => {
+  return (
+    <div>Scores_Type</div>
+  )
+}
+
+export default Scores_Type
