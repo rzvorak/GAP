7e69adee02d18b77868366755face41add8f38e2
Ryder Zvorak <ryderzvorak@gmail.com>
2025-03-16 10:20:04 -0500

    reports page base layout update

--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -1,32 +1,62 @@
 import React from 'react'
-import {Box, VStack, Heading} from '@chakra-ui/react'
+import { Box, VStack, Heading, useBreakpointValue } from '@chakra-ui/react'
 import Header from '../components/Header'
+import { useNavigate } from 'react-router-dom'
+import { FaArrowLeft } from 'react-icons/fa';
+
 
 const Reports = () => {
-  return (
-    <Box 
-            minH={"100vh"} 
-            maxW={"100vw"}
-            bg={"gray.100"}
-            color="gray.900" 
-        >
-        <Header></Header>
+  const disappearOnMin = useBreakpointValue({ "min": "none", "xxs": "flex" })
 
-        <VStack 
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate('/landing')
+  }
+
+  return (
+    <Box
+      minH={"100vh"}
+      maxW={"100vw"}
+      bg={"gray.100"}
+      color="gray.900"
+      display="flex"
+      flexDir={"column"}
+    >
+      <Header></Header>
+
+      <VStack 
+        flex="1"
         w="100%">
-            <Box 
-            w="100%"
-            h={{sm: "4rem"}}
-            display="flex"
-            alignItems={"center"}>
-                <Heading 
-                marginLeft="1rem"
-                color="gray.600"
-                fontSize="2xl"
-                fontWeight={"400"}
-                >Generate a Report</Heading>
-            </Box>
-        </VStack>
+        <Box
+          w="100%"
+          h={{ sm: "4rem" }}
+          display="flex"
+          alignItems={"center"}>
+          <Heading
+            marginLeft="1rem"
+            color="gray.600"
+            fontSize="2xl"
+            fontWeight={"400"}
+          >Generate a Report</Heading>
+        </Box>
+      </VStack>
+
+
+      <Box
+        w="100%"
+        display={disappearOnMin}
+        h="8rem"
+        paddingTop="2rem"
+        paddingBottom="2rem"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          cursor={"pointer"}
+          onClick={handleBack}>
+          <FaArrowLeft size="1.5rem" className='FaArrowLeft' />
+        </Box>
+      </Box>
 
     </Box>
   )
